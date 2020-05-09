0e2ea5221e007d482208ebc0a433989f1281cdbf
Y W J台式机 <1159014200@qq.com>
2020-05-09 09:47:38 +0800

    连接数据库

--- CodeGenerator/ConnectionModal/SQLConnectionModal.xaml.cs
+++ CodeGenerator/ConnectionModal/SQLConnectionModal.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +25,50 @@ namespace CodeGenerator.ConnectionModal
         public SQLConnectionModal()
         {
             InitializeComponent();
+            txt_server.Text = Environment.MachineName;//计算机名称
+        }
+
+        public string ConnectionString { get; set; }//连接字符串
+        //public int VerificationType { get; set; }
+
+        //连接数据库
+        private void Btn_Connection_Click(object sender, RoutedEventArgs e)
+        {
+            string txt_serverVal = txt_server.Text;//服务器名称
+            int txt_verification_typeVal = int.Parse(txt_verification_type.SelectedValue.ToString());//身份验证类型   Windows 身份认证  SQL Server 身份认证
+            string txt_accountVal = txt_account.Text;//登录名
+            string txt_pwdVal = txt_pwd.Password;//登录密码
+            if (txt_verification_typeVal == 0)
+            {
+                ConnectionString = string.Format("Data Source={0};Integrated Security=SSPI;", txt_serverVal);
+            }
+            else
+            {
+                ConnectionString = string.Format("Data Source={0};uid={1};pwd={2};", txt_serverVal, txt_accountVal, txt_pwdVal);
+            }
+
+            DbHelperSQL.ConnectionString = ConnectionString;
+            string tableName = "master..sysdatabases";
+            string fieldSelect = "name";
+            string strWhere = fieldSelect + " NOT IN ( 'master', 'model', 'msdb', 'tempdb', 'northwind','pubs' )";
+            DataTable dt = DbHelperSQL.GetDataTable(tableName, fieldSelect, strWhere);
+            if (dt != null)
+            {
+                MessageBox.Show("连接成功！");
+                txt_database.ItemsSource = dt.DefaultView;
+                txt_database.SelectedValuePath = fieldSelect;
+                txt_database.DisplayMemberPath = fieldSelect;
+                txt_database.SelectedIndex = 0;
+            }
+            
+        }
+
+
+        private void Btn_Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            string txt_databaseVal = txt_database.SelectedValue.ToString();//数据库
+            ConnectionString += "Initial Catalog:" + txt_databaseVal + ";";
+            DbHelperSQL.ConnectionString = ConnectionString;
         }
     }
 }
---
+++ Common/DbHelperSQL.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class DbHelperSQL
+    {
+        public static string ConnectionString { get; set; }
+        public static DataSet Query(string strSql)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                DataSet ds = new DataSet();
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(strSql, connection);
+                    sqlDataAdapter.Fill(ds);
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    throw new Exception(ex.Message);
+                }
+                return ds;
+            }
+        }
+
+        //选择一行数据
+        public static DataRow GetDataRow(string tableName, string fieldSelect, string strWhere)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.Append("select top 1 ");
+                strSql.Append("" + fieldSelect + " ");
+                strSql.Append("from " + tableName + " ");
+                if (strWhere != "")
+                {
+                    strSql.Append("where " + strWhere + "");
+                }
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(strSql.ToString(), connection);
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    sqlDataAdapter.Fill(dt);
+                    return dt.Rows[0];
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    throw new Exception(ex.Message);
+                }
+
+
+            }
+        }
+
+        public static DataTable GetDataTable(string tableName, string fieldSelect, string strWhere)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.Append("select ");
+                strSql.Append("" + fieldSelect + " ");
+                strSql.Append("from " + tableName + " ");
+                if (strWhere != "")
+                {
+                    strSql.Append("where " + strWhere + " ");
+                }
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(strSql.ToString(), connection);
+                    DataSet ds = new DataSet();
+                    sqlDataAdapter.Fill(ds);
+                    return ds.Tables[0];
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+    }
+}
