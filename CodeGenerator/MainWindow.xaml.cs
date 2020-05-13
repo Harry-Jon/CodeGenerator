514c8e17a49a99c29a1c3e24fe4a1562f8a211e1
Y W J台式机 <1159014200@qq.com>
2020-05-13 22:19:58 +0800

    树形数据测试

--- CodeGenerator/MainWindow.xaml.cs
+++ CodeGenerator/MainWindow.xaml.cs
@@ -23,6 +23,47 @@ namespace CodeGenerator
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Model.Table> tables = new List<Model.Table>()
+            {
+                new Model.Table{ TableName="表1"},
+                new Model.Table{ TableName="表2"}
+            };
+            List<Model.DataBase> dataBases = new List<Model.DataBase>()
+            {
+                new Model.DataBase{ DataBaseName="数据库1", TableList=tables},
+                new Model.DataBase{ DataBaseName="数据库2", TableList=tables}
+            };
+            List<Model.DataSourceTree> dataSourceTrees = new List<Model.DataSourceTree>()
+            {
+                new Model.DataSourceTree{ DataSourceName="服务器1", DatabaseList=dataBases},
+                new Model.DataSourceTree{ DataSourceName="服务器2", DatabaseList=dataBases}
+            };
+            //DataSourceTreeView.ItemsSource = dataSourceTrees;
+
+            List<Model.TreeObject> list = new List<Model.TreeObject>()
+            {
+                new Model.TreeObject{ Name="服务器1", Children=new List<Model.TreeObject>()
+                {
+                    new Model.TreeObject{ Name="数据库1",Children=new List<Model.TreeObject>()
+                    {
+                        new Model.TreeObject{ Name="表1",Children=null}
+                    } },
+                    new Model.TreeObject{ Name="数据库2",Children=new List<Model.TreeObject>()
+                    {
+                        new Model.TreeObject(){ Name="表2",Children=null}
+                    } }
+                } },
+
+            };
+            DataSourceTreeView.ItemsSource = list;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            View.ConnectionModal.SQLConnectionModal connectionModal = new View.ConnectionModal.SQLConnectionModal();
+            connectionModal.Show();
+
         }
     }
 }
---
+++ CodeGenerator/Model/DataSourceTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.Model
+{
+    public class DataSourceTree
+    {
+        public string DataSourceName { get; set; }
+        public List<DataBase> DatabaseList { get; set; }
+    }
+
+
+    public class Table
+    {
+        public string TableName { get; set; }
+    }
+    public class DataBase
+    {
+        public string DataBaseName { get; set; }
+        public List<Table> TableList { get; set; }
+    }
+
+
+    public class TreeObject
+    {
+        public string Name { get; set; }
+        public List<TreeObject> Children { get; set; }
+    }
+}
