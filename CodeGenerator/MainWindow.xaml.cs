ece0929d3f496c24b8f0c8dca9edc29535b80d4f
Y W J台式机 <1159014200@qq.com>
2020-05-16 09:41:27 +0800

    TreeView控件

--- CodeGenerator/MainWindow.xaml.cs
+++ CodeGenerator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CodeGenerator.Model;
+using Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,5 +35,7 @@ namespace CodeGenerator
             connectionModal.ShowDialog();
 
         }
+        
+
     }
 }
--- CodeGenerator/Model/DataSourceTree.cs
+++ CodeGenerator/Model/DataSourceTree.cs
@@ -9,11 +9,28 @@ namespace CodeGenerator.Model
 {
     public class DataSourceTree
     {
+        public DataSourceTree() 
+        {
+            Children = new List<DataSourceTree>();
+        }
         public string Name { get; set; }
-        public List<DataSourceTree> Children = new List<DataSourceTree>();
+        public List<DataSourceTree> Children { get; set; }
         public void Add(DataSourceTree node)
         {
             this.Children.Add(node);
         }
+    }
+
+    public class DepartmentModel
+    {
+        public List<DepartmentModel> Nodes { get; set; }
+        public DepartmentModel()
+        {
+            this.Nodes = new List<DepartmentModel>();
+            this.ParentId = 0;//主节点的父id默认为0
+        }
+        public int id { get; set; }//id
+        public string deptName { get; set; }//部门名称
+        public int ParentId { get; set; }//父类id
     }
 }
